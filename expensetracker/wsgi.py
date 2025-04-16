0eb06894c19914ef3ddd5a9b0fed7ebe5309e553
susheelbms129 <susheel.is22@bmsce.ac.in>
2025-04-17 00:04:26 +0530

    Add nltk stopwords download at startup

--- expensetracker/wsgi.py
+++ expensetracker/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
+import nltk
+nltk.download('stopwords')  # ✅ Download on server start
+
+
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'expensetracker.settings')
 
 application = get_wsgi_application()
