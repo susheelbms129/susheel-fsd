0b9676f1a2e5270ce3753dcc0c57ddff781d08a5
susheelbms129 <susheel.is22@bmsce.ac.in>
2025-05-24 17:26:14 +0530

    Update wsgi.py

--- expensetracker/wsgi.py
+++ expensetracker/wsgi.py
@@ -12,27 +12,12 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-# import nltk
-# nltk.download('stopwords')  # ✅ Download on server start
+import nltk
+nltk.download('stopwords')  # ✅ Download on server start
 
 
 
 
-# this below extra added 
-# Set a local nltk_data folder in your project directory
-nltk_data_dir = os.path.join(os.path.dirname(__file__), 'nltk_data')
-
-# Create directory if not exist
-os.makedirs(nltk_data_dir, exist_ok=True)
-
-# Append to nltk data search paths so it looks here first
-nltk.data.path.append(nltk_data_dir)
-
-# Try to find stopwords corpus, if missing download it here
-try:
-    nltk.data.find('corpora/stopwords')
-except LookupError:
-    nltk.download('stopwords', download_dir=nltk_data_dir)
 
 
 
